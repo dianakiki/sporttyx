8c291fd2b9bed19557382f0d23d37cf962ff7d9b
dgataullina <dgataullina@diasoft.ru>
2026-02-24 21:24:23 +0300

    env

--- backend/src/main/java/com/app/config/WebMvcConfig.java
+++ backend/src/main/java/com/app/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
         String uploadPath = Paths.get("uploads").toAbsolutePath().toString() + "/";
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://193.247.73.166", "http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
